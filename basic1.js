82aa43172098633d737ae920bffbc930221284a1
nidhibakher <nidhibakher@gmail.com>
2022-11-14 14:59:13 +0530

    file added

---
+++ basic1.js
@@ -0,0 +1,10 @@
+
+<html>
+    <body>
+        <h2>my first javascript </h2>
+        <button type ="button"
+        onclick="document.getElementbyId('demo').innerHTML=Date()">
+        Click me to display Date and Time.</button>
+        <p id="demo"></p>
+    </body>
+</html>
